4822bacc399b07926ee963887a820a6a8fbb509a
David Valleau <valleau@chromium.org>
2018-12-12 10:33:51 -0800

    Adding value_util library for common base::Value and JSON functions
    BUG=chromium:885915
    TEST=None
    
    Change-Id: Ibd1803f9e73f2f2d162873aa9c4974b2658b56a4
    Reviewed-on: https://chromium-review.googlesource.com/1374373
    Commit-Ready: David Valleau <valleau@chromium.org>
    Tested-by: David Valleau <valleau@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>

---
+++ value_util.cc
@@ -0,0 +1,34 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "value_util.h"
+
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
+#include <base/json/json_reader.h>
+#include <base/optional.h>
+#include <base/values.h>
+
+base::Optional<std::string> GetJSONContents(const std::string& file_path) {
+  std::string json_contents;
+  const base::FilePath path(file_path);
+  if (!base::ReadFileToString(path, &json_contents)) {
+    return {};
+  }
+  return json_contents;
+}
+
+std::unique_ptr<base::Value> GetJSONValue(const std::string& json_contents) {
+  base::JSONReader json_reader;
+  std::unique_ptr<base::Value> value = json_reader.Read(json_contents);
+  CHECK(value) << "Failed to parse JSON string";
+  return value;
+}
+
+const base::DictionaryValue* GetDictionary(const base::Value* value) {
+  const base::DictionaryValue* dict;
+  CHECK(value->GetAsDictionary(&dict)) << "Failed to extract value of type "
+                                       << value->type() << " as dictionary";
+  return dict;
+}
---
+++ value_util.h
@@ -0,0 +1,23 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef __VALUD_UTIL_H__
+#define __VALUD_UTIL_H__
+
+#include <base/json/json_reader.h>
+#include <base/optional.h>
+#include <base/values.h>
+
+// Attempt to load the contents of the JSON file located at |file_path| and
+// return the contents in a string.
+base::Optional<std::string> GetJSONContents(const std::string& file_path);
+
+// Use a JSONReader to parse |json_contents| and return a pointer to the
+// underlying Value object.
+std::unique_ptr<base::Value> GetJSONValue(const std::string& json_contents);
+
+// Utility function for converting |value| into a DictionaryValue object.
+const base::DictionaryValue* GetDictionary(const base::Value* value);
+
+#endif  // __VALUD_UTIL_H__
